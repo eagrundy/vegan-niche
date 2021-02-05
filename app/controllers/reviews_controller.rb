1f2553e6017fb56a9e05f1e9f80c3ac1b0fe6247
eagrundy <ericamalakian@gmail.com>
2021-02-05 15:40:19 -0800

    Improved Index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,19 @@
 class ReviewsController < ApplicationController
-    # before_action :current_user, only: [:destroy]
-    before_action :set_restaurant, only: [:new, :create, :edit, :update]
+    before_action :set_restaurant, only: [:index, :new, :create, :edit, :update]
     before_action :set_review, only: [:edit, :update, :destroy]
-
-
+    
+    def index
+       
+        @reviews = @restaurant.reviews.order(id: :desc)
+        # @review_average = Review.where(:restaurant_id => params[:restaurant_id]).average(:rating).to_i
+        @review_average = @restaurant.reviews.average(:rating)
+    end
+    
     def new
         if current_user && logged_in?
             @review = Review.new
-            
+            # @review = @restaurant.reviews.build
+            # test this out
         else
             flash[:alert] = "Please log in to review a restaurant!"
             redirect_to login_path
@@ -20,19 +26,18 @@ class ReviewsController < ApplicationController
         @review.user_id = current_user.id
         if @review.valid?
             @review.save
+            # try to use just .save
+            # current_user.review.build
         else
             flash[:alert] = "Review can't be blank!"
         end
-        redirect_to restaurant_path(@restaurant)
+        redirect_to restaurant_reviews_path(@restaurant)
     end
 
     def edit
-        # @review = @restaurant.reviews
     end
 
     def update
-        # @review.restaurant_id = @restaurant.id
-        # @review.user_id = current_user.id
         if @review.update(review_params)
             
         else
@@ -68,13 +73,6 @@ class ReviewsController < ApplicationController
         end
     end
 
-      # def index
-    #     # if params[:restaurant_id]
-    #     #     @reviews = @restaurant.reviews
-    #     # else 
-    #     @reviews = Review.all
-    #     # end
-    # end
 
     # def show
     #     @review = Review.find_by_id(params[:id])
